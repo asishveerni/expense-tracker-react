0805b6ebb2064026b5a5336af5851c42f43bdf22
asishveerni <17pa1a04h1@vishnu.edu.in>
2020-07-22 17:05:50 +0530

    Updates

---
+++ precache-manifest.53e50466d2289bea0bd732c379f991cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a334ffaf728813bc886a54b89f857acf",
+    "url": "/expense-tracker-react/index.html"
+  },
+  {
+    "revision": "6287b594de7ef44f08d3",
+    "url": "/expense-tracker-react/static/css/main.3e1c1541.chunk.css"
+  },
+  {
+    "revision": "d274357ae37698f69cd8",
+    "url": "/expense-tracker-react/static/js/2.87902a96.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/expense-tracker-react/static/js/2.87902a96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6287b594de7ef44f08d3",
+    "url": "/expense-tracker-react/static/js/main.68f8da53.chunk.js"
+  },
+  {
+    "revision": "67a4669715835298d50d",
+    "url": "/expense-tracker-react/static/js/runtime-main.3587cd2a.js"
+  }
+]);
